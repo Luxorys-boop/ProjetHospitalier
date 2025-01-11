cffc70beb1bcb026a82fda65bedc9e9e95373aff
KyHzZzz <kylian37100@gmail.com>
2025-01-11 18:43:38 +0100

    modif shift

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,6 @@ import AppShifts from './composantsShifts/AppShifts.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    
+    <AppShifts />
   </StrictMode>,
 )
